33d5662f9acf6d4242410d719f08bd89db13c13e
al375837 <al375837@uji.es>
2021-03-20 13:36:39 +0100

    mejoras y etiquetado

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from app.endpoints.dispositivos import router as router_dispositivos
 from app.endpoints.establecimientos import router as router_establecimientos
@@ -10,3 +11,7 @@ app.include_router(router_dispositivos)
 app.include_router(router_security)
 app.include_router(router_establecimientos)
 
+
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse("docs")

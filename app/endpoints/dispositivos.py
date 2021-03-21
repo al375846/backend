99731cd526cd5b788f37ed18a5b6be224da65b79
al375846 <al375846@uji.es>
2021-03-21 16:19:52 +0100

    asociar dispostivos a establecimiento

--- app/endpoints/dispositivos.py
+++ app/endpoints/dispositivos.py
@@ -1,7 +1,9 @@
-from fastapi.routing import APIRouter
+from fastapi import APIRouter, Depends
 from app.models.dispositivo import DispositivoCreate, DispositivoDB
 from app.db.db import db
 from bson import ObjectId
+from app.models.gerente import Gerente
+from app.utils.security import get_current_gerente
 
 router = APIRouter(prefix="/dispositivo", tags=["Dispositivos"])
 
@@ -13,19 +15,19 @@ async def add_dispositivo(dispositivo: DispositivoCreate):
     return disp
 
 @router.get("/{dispositivo_id}")
-async def retrieve_dispositivo(dispositivo_id):
+async def retrieve_dispositivo(dispositivo_id:str, gerente: Gerente = Depends(get_current_gerente)):
     disp = await db.motor.find_one(DispositivoDB, DispositivoDB.id == ObjectId(dispositivo_id))
     return disp
 
 @router.put("/{dispositivo_id}/activar")
-async def activate_dispositivo(dispositivo_id, activado:bool):
+async def activate_dispositivo(dispositivo_id:str, activado:bool, gerente: Gerente = Depends(get_current_gerente)):
     disp = await db.motor.find_one(DispositivoDB, DispositivoDB.id == ObjectId(dispositivo_id))
     disp.activado = activado
     await db.motor.save(disp)
     return disp
 
 @router.put("/{dispositivo_id}/conectar")
-async def connect_dispositivo(dispositivo_id, conectado:bool):
+async def connect_dispositivo(dispositivo_id:str, conectado:bool, gerente: Gerente = Depends(get_current_gerente)):
     disp = await db.motor.find_one(DispositivoDB, DispositivoDB.id == ObjectId(dispositivo_id))
     disp.conectado = conectado
     await db.motor.save(disp)
--- app/models/dispositivo.py
+++ app/models/dispositivo.py
@@ -1,7 +1,9 @@
 from app.enums.tipodispositivo import TipoDispositivo
 from datetime import datetime
-from odmantic import Model
+from odmantic import Model, Reference
 from pydantic.main import BaseModel
+from app.models.establecimiento import EstablecimientoDB
+from typing import Optional
 
 
 class DispositivoDB(Model):
@@ -10,6 +12,7 @@ class DispositivoDB(Model):
     activado: bool
     conectado: bool
     tipo: TipoDispositivo
+    establecimiento: Optional[str] = None
 
 class DispositivoCreate(BaseModel):
     modelo: str

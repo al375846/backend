44bff6947843df229e2356a50a04b99b6b2528b8
al375837 <al375837@uji.es>
2021-03-21 18:52:48 +0100

    Formateado código

--- app/endpoints/dispositivos.py
+++ app/endpoints/dispositivos.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
-from app.models.dispositivo import DispositivoCreate, DispositivoDB
-from app.db.db import db
 from bson import ObjectId
-from app.models.gerente import Gerente
+from fastapi import APIRouter, Depends
+
+from app.db.db import db
+from app.models.dispositivo import DispositivoCreate, DispositivoDB
 from app.utils.security import get_current_gerente
 
 router = APIRouter(prefix="/dispositivo", tags=["Dispositivos"])
@@ -16,13 +16,13 @@ async def add_dispositivo(dispositivo: DispositivoCreate):
 
 
 @router.get("/{dispositivo_id}")
-async def retrieve_dispositivo(dispositivo_id: str, gerente: Gerente = Depends(get_current_gerente)):
+async def retrieve_dispositivo(dispositivo_id: str, _=Depends(get_current_gerente)):
     disp = await db.motor.find_one(DispositivoDB, DispositivoDB.id == ObjectId(dispositivo_id))
     return disp
 
 
 @router.put("/{dispositivo_id}/activar")
-async def activate_dispositivo(dispositivo_id: str, activado: bool, gerente: Gerente = Depends(get_current_gerente)):
+async def activate_dispositivo(dispositivo_id: str, activado: bool, _=Depends(get_current_gerente)):
     disp = await db.motor.find_one(DispositivoDB, DispositivoDB.id == ObjectId(dispositivo_id))
     disp.activado = activado
     await db.motor.save(disp)
@@ -30,7 +30,7 @@ async def activate_dispositivo(dispositivo_id: str, activado: bool, gerente: Ger
 
 
 @router.put("/{dispositivo_id}/conectar")
-async def connect_dispositivo(dispositivo_id: str, conectado: bool, gerente: Gerente = Depends(get_current_gerente)):
+async def connect_dispositivo(dispositivo_id: str, conectado: bool, _=Depends(get_current_gerente)):
     disp = await db.motor.find_one(DispositivoDB, DispositivoDB.id == ObjectId(dispositivo_id))
     disp.conectado = conectado
     await db.motor.save(disp)

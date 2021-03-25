25d4271c0d57554fb914c44594028caaa5167b93
Aalmr <almar.b.miller@gmail.com>
2021-03-25 12:15:42 +0100

    Arreglado bug

--- app/cam/__init__.py
+++ app/cam/__init__.py
@@ -7,11 +7,11 @@ vc = VideoCapture(0)
 frame_period = 100
 url = 'http://127.0.0.1:8000/images/'
 """url = 'https://syskaoh.herokuapp.com/images'"""
-img_name = str(datetime.now().time()) + '.jpg'
 
 while True:
     for i in range(frame_period):
         next_vc, frame = vc.read()
+    img_name = str(datetime.now().time()) + '.jpg'
     imshow("Device0", frame)
     imwrite(img_name, frame)
     file = {'file': open('./' + img_name, 'rb')}

c82fe5c273492e0cde06e6902eb271dc0f430c16
jaume <al375837@uji.es>
2021-03-29 17:03:09 +0200

    agregado informes

--- app/endpoints/mediciones.py
+++ app/endpoints/mediciones.py
@@ -1,10 +1,11 @@
-from bson import ObjectId
+from app.enums.medicion import TipoMedicion
+from odmantic import ObjectId, query
 from fastapi import Depends, HTTPException
 from fastapi.routing import APIRouter
 from starlette import status
 
 from app.db.db import db
-from app.models.medicion import Medicion
+from app.models.medicion import Medicion, InformeMedicionRet, MedicionRet
 from app.models.establecimiento import EstablecimientoDB
 from app.models.gerente import Gerente
 from app.utils.security import get_current_gerente
@@ -14,14 +15,38 @@ from datetime import datetime
 router = APIRouter(prefix="/medicion",
                    tags=["Mediciones"])
 
-@router.get("/aforo/{establecimiento_id}")
-async def obtener_medicion_aforo(establecimiento_id:str, gerente: Gerente = Depends(get_current_gerente)):
-    establecimiento = await db.motor.find_one(EstablecimientoDB, EstablecimientoDB.id == ObjectId(establecimiento_id))
-    if establecimiento is None:
-        raise HTTPException(detail="Ese establecimiento no existe", status_code=status.HTTP_404_NOT_FOUND)
-    if establecimiento.mediciones is None:
-        return {'contenido':'-1'}
-    else:
-        ultima=establecimiento.mediciones[-1].contenido
-        return {'contenido':ultima}
 
+@router.get("/aforo/{establecimiento_id}",response_model=MedicionRet)
+async def obtener_medicion_aforo(establecimiento_id: ObjectId, gerente: Gerente = Depends(get_current_gerente)):
+
+    establecimiento = await db.motor.find_one(EstablecimientoDB, EstablecimientoDB.id == establecimiento_id)
+    if establecimiento is None:
+        raise HTTPException(detail="Ese establecimiento no existe",
+                            status_code=status.HTTP_404_NOT_FOUND)
+    if establecimiento.mediciones is None:
+        return MedicionRet()
+    else:
+        ultima = filtra_fecha(establecimiento.mediciones,TipoMedicion.aforo)[-1].contenido
+        return MedicionRet(contenido = ultima)
+
+
+@router.get("/informe/aforo/{establecimiento_id}",response_model=InformeMedicionRet)
+async def obtener_mediciones_aforo(*, establecimiento_id: ObjectId, fecha_ini: datetime = None, fecha_fin: datetime = None,
+                                   gerente: Gerente = Depends(get_current_gerente)):
+    establecimiento = await db.motor.find_one(EstablecimientoDB,query.and_(EstablecimientoDB.gerente == gerente.id,EstablecimientoDB.id == establecimiento_id))
+    
+    if establecimiento is None:
+        raise HTTPException(detail="Ese establecimiento no existe",
+                            status_code=status.HTTP_404_NOT_FOUND)
+    if establecimiento.mediciones is None:
+        return InformeMedicionRet()
+    else:
+        mediciones = establecimiento.mediciones
+        mediciones = filtra_fecha(mediciones,TipoMedicion.aforo,fecha_ini,fecha_fin)
+        return InformeMedicionRet(contenido= mediciones)
+
+
+def filtra_fecha(mediciones: Medicion, tipo: TipoMedicion, fecha_ini: datetime = None, fecha_fin: datetime = None):
+    
+    return list(filter(lambda medicion:medicion.tipo_medicion == tipo and (fecha_fin is None or medicion.fecha <= fecha_fin)
+                and (fecha_ini is None or medicion.fecha >= fecha_ini),mediciones))
--- app/models/medicion.py
+++ app/models/medicion.py
@@ -1,9 +1,20 @@
 from datetime import datetime
+from typing import List, Optional, Union
 
 from odmantic import EmbeddedModel
+from pydantic.main import BaseModel
 from app.enums.medicion import TipoMedicion
 
 class Medicion(EmbeddedModel):
-    identificador: str
+    tipo_medicion: TipoMedicion
+    identificador_disp: str
     fecha: datetime = datetime.now()
     contenido: str
+
+
+
+class InformeMedicionRet(BaseModel):
+    contenido: Optional[List[Medicion]] = None
+
+class MedicionRet(BaseModel):
+    contenido: Optional[str] = None

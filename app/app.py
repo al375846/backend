f5179dfe7c37e2e31a40394012229433c2bcf9e3
jaume <al375837@uji.es>
2021-05-04 16:38:11 +0200

    arreglo imports

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
 from app.db.db import start_db, close_mongo_connection
-from app.endpoints.dispositivos import router as router_dispositivos
-from app.endpoints.establecimientos import router as router_establecimientos
+from app.endpoints.utils import router as router_utils
 from app.endpoints.gestion import router as router_gestion
 from app.endpoints.security import router as router_security
 from app.endpoints.mediciones import router as router_mediciones
 from app.endpoints.notificaciones import router as router_notificaciones
-from app.endpoints.utils import router as router_utils
+from app.endpoints.establecimientos import router as router_establecimientos
+from app.endpoints.dispositivos import router as router_dispositivos
 from app.endpoints.sugerencias import router as router_sugerencias
 from app.endpoints.incidencia import router as router_incidencias
 
